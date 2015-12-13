75bcb052b847f2c6fa38af928778c9a5cf8bc826
josellausas <jose@josellausas.com>
2015-12-13 13:39:22 -0600

    Dissabled the console.

--- web.lua
+++ web.lua
@@ -1,46 +1,54 @@
-local lapis = require "lapis"
+local lapis   = require "lapis"
 local console = require("lapis.console")
-local config = require("lapis.config").get()
-local mqtt = require("mqtt")
+local config  = require("lapis.config").get()
+local mqtt    = require("mqtt")
 
 -- Do the app config things
 local app = lapis.Application()
 app:enable("etlua")
+
+
 app.layout = require "views.layout"		-- Sets the layout we are using.
 
 
 -- Override what happens when there is a default route.
 app.default_route = function ( self )
+	
 	-- There is no trailing slash removal at this step!!!
 	ngx.log(ngx.NOTICE, "User 404 path " .. self.req.parsed_url_path)
 
 	return lapis.Application.default_route(self)
 end
 
+
+
 -- Handle 404s
 app.handle_404 = function( self )
-
+	-- Returns the code 404
 	return { status = 404, layout = false, "Not Found!" }
 end
 
 -- Handle errors
 app.handle_error = function(self, err, trace)
+	-- Logs to the nginx console
 	ngx.log(ngx.NOTICE, "Lapis error: " .. err .. ": " .. trace)
+
+	-- Handle the erros with Lapis internaly.
 	lapis.Application.handle_error(self, err, trace)
+
+	-- TODO: Crashlytics?
 end
 
 
 -- Figures out the html for the index.
 local renderIndex = function()
+	
 	local theString = "<h2>version</h2> " .. require("lapis.version")
 
 	return theString
 end
 
--- Maps a URL to a function that returns HTML
-app:get("/", function(self)
-	return { render  = "index" , status = 200, content_type = "text/html" }
-end)
+
 
 -- Muestra una lista
 app:get("/lista", function(self)
@@ -70,16 +78,21 @@ local function generateJSON(self)
 	return response
 end
 
+
+-- INDEX
+app:get("/", function(self)
+	return { render  = "index" , status = 200, content_type = "text/html" }
+end)
+
+
 app:get("/testjson", generateJSON)
 
 -- Muestra la version
 app:get("/version", renderIndex)
 
+
 -- The LUA CONSOLE FTW!!!
+-- app:match("/console", console.make({env="masterDeveloper"}))
 
-app:match("/console", console.make({env="all"}))
-
-
-
--- Starts serving the app
+-- Serves a lapis web app.
 lapis.serve(app)
