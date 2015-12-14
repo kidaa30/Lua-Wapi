b3953e8ac9c0486425c11cbeb52697d847e23094
josellausas <jose@josellausas.com>
2015-12-14 02:19:12 -0600

    Fast commit

--- testData.lua
+++ testData.lua
@@ -1,6 +1,11 @@
 testData = {
 	siteName = "Lua-Wapi Framework",
 	dashboardTitle = "Mobile Decisions by Llau",
+	mensaje = {
+        from = "Jose Llausas",
+        whenfrom = "Yesterday",
+        message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend..."
+    }
 
 	
 }
