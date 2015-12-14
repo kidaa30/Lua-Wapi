14c81de84b76e3fa34bfde0e9db4c0e1f2279401
josellausas <jose@josellausas.com>
2015-12-13 22:32:34 -0600

    Yeah yeah yeah.

--- web.lua
+++ web.lua
@@ -90,7 +90,7 @@ app:get("/lista", function(self)
 	-- Loop la respuesta y agregarla a la l
 	for k, v in pairs( res ) do
    		n = n+1
-   		listaLoad[n] = k
+   		table.insert(listaLoad, k)
 	end
 
 	-- Esta es la lista que se renderea
