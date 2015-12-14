2d7ab6417f31f88d43ded63a2a29d164559679e3
josellausas <jose@josellausas.com>
2015-12-13 21:27:38 -0600

    Maybe.

--- config.lua
+++ config.lua
@@ -2,5 +2,11 @@ local config
 config = require("lapis.config").config
 return config("heroku", function()
   port(os.getenv("PORT"))
-  return postgresql_url(os.getenv("DATABASE_URL"))
+  postgresql_url(os.getenv("DATABASE_URL"))
+  return postgres(function()
+    host("ec2-54-83-59-203.compute-1.amazonaws.com")
+    user("wddcthddvouvtr")
+    password("_EsJ9XVoYVSYXDWbUDOTQPdrph")
+    return database("d2k28tn5s3orl5")
+  end)
 end)
