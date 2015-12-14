838e795ef36dbeb89221b3351d976805038a98dd
josellausas <jose@josellausas.com>
2015-12-13 21:18:04 -0600

    OK.

--- config.lua
+++ config.lua
@@ -1,9 +1,6 @@
--- config
-local config = require("lapis.config")
-
-return config({"heroku, development", "production"}, {
-	port = os.getenv("PORT"),
-	postgresql_url = os.getenv("DATABASE_URL"),
-})
-
-
+local config
+config = require("lapis.config").config
+return config("heroku", function()
+  port(os.getenv("PORT"))
+  return postgresql_url(os.getenv("DATABASE_URL"))
+end)
