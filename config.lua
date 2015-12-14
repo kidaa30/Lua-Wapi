cb7ee59e6cdadab55d70e8bfd3ecf2ffe10bcc2c
josellausas <jose@josellausas.com>
2015-12-13 20:29:39 -0600

    OK.

--- config.lua
+++ config.lua
@@ -1,11 +1,22 @@
-local config
-config = require("lapis.config").config
-return config("heroku", function()
-  postgresql_url(os.getenv("DATABASE_URL"))
-  return postgres(function()
-    host("ec2-54-83-59-203.compute-1.amazonaws.com")
-    user("wddcthddvouvtr")
-    password("_EsJ9XVoYVSYXDWbUDOTQPdrph")
-    return database("d2k28tn5s3orl5")
-  end)
-end)
+-- config
+local config = require("lapis.config")
+
+-- return config("heroku", function()
+-- 	port(os.getenv("PORT"))
+-- 	return postgresql_url(os.getenv("DATABASE_URL"))
+-- end)
+
+return config("heroku", {
+	port = os.getenv("PORT"),
+	postgresql_url = os.getenv("DATABASE_URL"),
+	postgres = {
+		host = "ec2-54-83-59-203.compute-1.amazonaws.com"
+		url = os.getenv("DATABASE_URL"),
+		port = "5432",
+		user = "wddcthddvouvtr",
+		password = "_EsJ9XVoYVSYXDWbUDOTQPdrph",
+		database = "d2k28tn5s3orl5",
+	},
+})
+
+
